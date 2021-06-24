b622f259ad432b6b19ad212ddfc7dc0afd9585b4
DFDIEGOH <dfdiegohernandez95@gmail.com>
2021-06-24 12:58:39 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  test() {
+    console.log("Entra");
+  }
   title = 'angular-tour-of-heroes';
 }
